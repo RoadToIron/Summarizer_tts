1b8131ab896d8d1a0fdcf18ec4c4a33df8f29343
RoadToIron <116758133+RoadToIron@users.noreply.github.com>
2025-09-22 19:58:36 +0000

    changed the pipeline to summarization

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import gradio as gr
 import tensorflow as tf
 
 #load the gpt model
-talker = pipeline("text-to-speech", model="espnet/fastspeech2_conformer_hifigan")
+#talker = pipeline("text-to-speech", model="microsoft/VibeVoice-1.5B")
 answerer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 def summarize_and_tts(text):
@@ -11,14 +11,16 @@ def summarize_and_tts(text):
     if not text:
         return "please enter your text >:(", None
     else:
-        summary = answerer(text, max_length=100, min_length=0, do_sample=False)[0][text]
+        summary = answerer(text, max_length=100, min_length=0, do_sample=False)[0]['summary_text']
     
         # Convert the summary to speech
-        tts_output = talker(summary)
+        #tts_output = talker(summary)
     
-        return summary, tts_output['audio']
+        return summary #tts_output['audio']
+
 
 # the Gradio Interface
+
 with gr.Blocks() as demo:
     text_input = gr.Textbox(label="Enter text to summarize and convert to speech", lines=10)
     summary_output = gr.Textbox(label="Summary")
